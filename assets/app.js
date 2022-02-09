5ee99395828ace2710af3346cca8737bb44e06eb
troat9824 <troat9824@gmail.com>
2022-02-09 13:27:22 -0600

    added JS from Ken

---
+++ assets/app.js
@@ -0,0 +1,38 @@
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
+$('#brewerform') .on("submit", function (events) {
+    events.preventDefault()
+    var city_input = $('#brewer').val();
+    fetch("https://api.openbrewerydb.org/breweries?by_city=" + city_input, {
+        "method": "GET",
+        "headers": {
+        }
+    })
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            console.log(data)
+            $("#response").html("<h1>Breweries in " + city_input + "</h1>")
+            data.forEach(function (obj) {
+                var brewery_name = obj.id
+                brewery_name = brewery_name.replaceAll("-", " ")
+                brewery_name = toTitleCase(brewery_name);
+                $("#response").append(`
+                <a href =${obj.website_url || "#"}> 
+                    <p>Name:  ${brewery_name}  </p>
+                </a>
+                `);
+                console.log(obj.id);
+
+
+            });
+
+        })
+});
